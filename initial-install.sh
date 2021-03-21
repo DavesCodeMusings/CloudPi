75174e3fb3a4164577f9ff1598b4197b84690d31
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2021-03-21 00:53:18 -0500

    Update initial-install.sh

--- initial-install.sh
+++ initial-install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-# Run this script once to update the OS pacakages, install Ansible, and reboot.
+# Run this script once to install Ansible, update the OS pacakages, and reboot.
 #
+[ "$(command -v ansible)" == "" ] || ( echo "Ansible is already installed." ; exit 1; )
 sudo apt update
+sudo apt install python3-pip
+sudo pip3 install ansible
 sudo apt upgrade
-sudo apt install ansible
 
 echo -n "Ready to reboot [y/N]? "
 read REPLY
 if [ "$REPLY" == "y" || "$REPLY" == "Y" ]; then
   sudo shutdown -r now
- fi
+fi
