737f0693e4ebb4a446aad72d9975af8f77e2be36
Dave Horton <dhorton668@outlook.com>
2021-03-21 09:25:56 -0500

    Ansible pip install

---
+++ ansible-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Run this script first to install the latest Ansible using Python pip.
+# The version from pip is newer than the apt repository and has features
+# needed to install the community.docker collection from Ansible Galaxy.
+#
+if [ "$(command -v ansible)" == "" ]; then
+  sudo apt update
+  sudo apt install python3-pip
+  sudo pip3 install ansible
+else
+  echo "Ansible is already installed."
+fi
