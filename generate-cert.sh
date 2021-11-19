4a0f2339a71788f303c48c58c58758a96b4b93de
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2021-11-18 23:27:40 -0600

    Update generate-cert.sh

--- generate-cert.sh
+++ generate-cert.sh
@@ -5,11 +5,17 @@
 #
 HOSTNAME=$(hostname)
 
+if ! [ -f ./${HOSTNAME}.cnf ]; then
+  echo "Can't find config file. Expecting: ${HOSTNAME}.cnf"
+  exit 1
+fi
+
 if [ -e /etc/ssl/certs/${HOSTNAME}.crt ]; then
   echo "Found an existing certificate: /etc/ssl/certs/${HOSTNAME}.crt"
 fi
+
 echo -n "Create a new SSL key pair for $HOSTNAME [y/N]? "
 read REPLY
 if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-  openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout /etc/ssl/private/${HOSTNAME}.key -days 365 -out /etc/ssl/certs/${HOSTNAME}.crt -config ${HOSTNAME}.cnf
+  sudo openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout /etc/ssl/private/${HOSTNAME}.key -days 365 -out /etc/ssl/certs/${HOSTNAME}.crt -config ${HOSTNAME}.cnf
 fi
